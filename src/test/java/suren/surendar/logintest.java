b6c2dacfa17891e6ed5a40c0ba0a1389b3a4a891
suren3298 <surenvijay5@gmail.com>
2022-01-07 17:30:35 +0530

    initial commit

---
+++ src/test/java/suren/surendar/logintest.java
@@ -0,0 +1,48 @@
+
+
+
+	package suren.surendar;
+	import static org.testng.AssertJUnit.assertEquals;
+	import static org.testng.AssertJUnit.assertTrue;
+	import org.testng.Assert;
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.annotations.AfterMethod;
+	import org.testng.annotations.BeforeMethod;
+	import org.testng.annotations.Test;
+	import suren.surendar.Login;
+	public class logintest {
+	 
+		@Test
+		public void verifyLogo() {
+		System.setProperty("webdriver.chrome.driver",
+		 "C:\\Users\\SUREN\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		 
+		 WebDriver driver= new ChromeDriver();
+		 Login loginpage = new Login(driver);
+		 driver.manage().window().maximize();
+		 driver.get("https://opensource￾demo.orangehrmlive.com/index.php/auth/validateCredentials");
+		 boolean flag=loginpage.validateLogo();
+		 Assert.assertTrue(flag);
+		}
+		 
+		@Test
+		public void verifyLogin() {
+		System.setProperty("webdriver.chrome.driver",
+		 "C:\\Users\\SUREN\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		 
+		 WebDriver driver= new ChromeDriver();
+		 Login loginpage = new Login(driver);
+		 driver.manage().window().maximize();
+		 driver.get("https://opensource￾demo.orangehrmlive.com/index.php/auth/validateCredentials");
+		 loginpage.login();
+		 
+		 String actualURL= driver.getCurrentUrl();
+		
+		 String expectedURL= "https://opensourcedemo.orangehrmlive.com/index.php/dashboard";
+		 Assert.assertEquals(actualURL, expectedURL);
+		}
+		// @AfterMethod
+		// public void tearDown() {
+		// driver.close();
+		}
